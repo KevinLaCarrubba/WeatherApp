033cf22a7c80f1704ec175921aff58a869b212b4
Kevin LaCarrubba <kevinlacarrubba@KevinsMacStudio.fios-router.home>
2022-08-25 12:48:02 -0400

    fixed hourly array

--- src/components/HourlyDay/index.js
+++ src/components/HourlyDay/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState, useCallback } from 'react'
 import { SearchContext } from '../../context/SearchContext'
 import { format } from 'date-fns'
 
@@ -10,38 +10,54 @@ const HourlyDay = () => {
     const { searchInfo } = useContext(SearchContext)
     const date = new Date()
     const currentHour = date.getHours()
+    const [hourlyArray, setHourlyArray] = useState([])
 
+    const createHoursArray = useCallback(() => {
+        const dayOne = searchInfo?.forecast?.forecastday[0]?.hour
+
+        if (currentHour !== 0) {
+            const dayOneSlice =
+                searchInfo?.forecast?.forecastday[0]?.hour.slice(
+                    currentHour,
+                    dayOne.length
+                )
+            const dayTwoSlice = searchInfo?.forecast.forecastday[1]?.hour.slice(
+                0,
+                currentHour
+            )
+            setHourlyArray(dayOneSlice.concat(dayTwoSlice))
+        }
+    }, [currentHour, searchInfo?.forecast?.forecastday])
+    useEffect(() => {
+        createHoursArray()
+    }, [createHoursArray])
+    console.log(hourlyArray)
     return (
         <div className={styles.container}>
             <div className={styles.infoContainer}>
-                {searchInfo?.forecast?.forecastday[0]?.hour
-                    .splice(0, currentHour)
-                    .map(info => {
-                        return (
-                            <div className={styles.info} key={info.time_epoch}>
-                                <div className={styles.time}>
-                                    {format(new Date(info.time), 'haaa')}
-                                </div>
-                                <div className={styles.logo}>
-                                    {renderIcon(
-                                        info.condition.code,
-                                        info.is_day
-                                    )}
-                                </div>
-                                <div className={styles.temp}>
-                                    {Math.round(info.temp_f)}°F
-                                </div>
-                                <div className={styles.condition}>
-                                    {info.condition.text}
-                                </div>
-                                <div className={styles.rain}>
-                                    <GiWaterDrop className={styles.waterDrop} />
-                                    {info.chance_of_rain}%
-                                </div>
+                {hourlyArray.map(info => {
+                    return (
+                        <div className={styles.info} key={info.time_epoch}>
+                            <div className={styles.time}>
+                                {format(new Date(info.time), 'haaa')}
                             </div>
-                        )
-                    })}
-                {currentHour !== 0 && (
+                            <div className={styles.logo}>
+                                {renderIcon(info.condition.code, info.is_day)}
+                            </div>
+                            <div className={styles.temp}>
+                                {Math.round(info.temp_f)}°F
+                            </div>
+                            <div className={styles.condition}>
+                                {info.condition.text}
+                            </div>
+                            <div className={styles.rain}>
+                                <GiWaterDrop className={styles.waterDrop} />
+                                {info.chance_of_rain}%
+                            </div>
+                        </div>
+                    )
+                })}
+                {/* {currentHour !== 0 && (
                     <>
                         {searchInfo?.forecast.forecastday[1]?.hour
                             .slice(0, currentHour)
@@ -79,7 +95,7 @@ const HourlyDay = () => {
                                 )
                             })}
                     </>
-                )}
+                )} */}
             </div>
         </div>
     )
